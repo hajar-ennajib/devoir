6564c251d51e531f063a260d8f4fee90a8eccb7e
hajar-ennajib <hajaren516@gmail.com>
2024-10-20 14:51:05 +0100

    code.devoir.cpp

--- code.devoir.cpp
+++ code.devoir.cpp
@@ -2,7 +2,7 @@
 #include<stdexcept>
 #include<iostream>
 using namespace std ;
-void testFonction(const vector<int>& params){
+void test(const vector<int>& params){
     cout<<"fonction appelee avec "<<params.size()<<" parametres"<<endl;
 }
 int main()
@@ -10,7 +10,7 @@ int main()
     try{
         for(size_t i=1; ;i*=3){
             vector<int>params(i,1);
-            testFonction(params);
+            test(params);
         }
     }catch (const bad_alloc& e){
         cout<<"error :"<<e.what()<<endl;
